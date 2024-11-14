798c0b8c382411d966c2ed63e4e5dfad6c466111
MunteanuMarian19 <marijean_marian93@yahoo.com>
2024-11-14 13:28:40 +0200

    Initial commit for frontend

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// module.exports = {
+//   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
